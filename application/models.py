290960d8ff44f0ff2155437fdb67bd2d571237e9
PhillipHage202 <73121315+PhillipHage202@users.noreply.github.com>
2020-11-08 17:24:11 +0000

    Update models.py

--- application/models.py
+++ application/models.py
@@ -2,5 +2,5 @@ from application import db
 
 class Todo (db.Model):
     id = db.Column(db.Integer, primary_keys=True)
-    To_do = db.Column(db.String(30), unique=True) 
-    finished = db.Column(db.Boolean, default=False)
+    task = db.Column(db.String(30), unique=True) 
+    complete = db.Column(db.Boolean, default=False)
