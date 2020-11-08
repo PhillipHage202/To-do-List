5379be1b71d45becb96ae083169b814a18a41bd3
PhillipHage202 <73121315+PhillipHage202@users.noreply.github.com>
2020-11-08 17:31:23 +0000

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,6 +1,6 @@
 from application import db
 
-class Todo (db.Model):
+class Todos (db.Model):
     id = db.Column(db.Integer, primary_keys=True)
     task = db.Column(db.String(30), unique=True) 
     complete = db.Column(db.Boolean, default=False)
