530806c265103264ed211a5919e04cff3a27e029
PhillipHage202 <73121315+PhillipHage202@users.noreply.github.com>
2020-11-08 17:31:40 +0000

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,54 @@
-from flask import render_template
-from application.models import Todo
+from flask import render_template, redirect, url_for, request
 
-@app.route('/add')
-def add ():
-    new_todo= Todo(To_do= 'New To'
-    db.session.add (new_todo)
-    db.session.commit ()
-    return "Added new To do"
+from application import db, app
+from application.models import Todos
+from application.forms import ToDoForm
 
-@app.route('/update/<name>')
-def update (name):
+@app.route('/')
+def index():
+    all_todos = Todos.query.all()
+    return render_template('index.html', all_todos=all_todos)
 
+@app.route('/add', methods=['GET','POST'])
+def add():
+    form = ToDoForm()
+    if form.validate_on_submit():
+        new_todo = Todos(task=form.task.data)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+@app.route('/complete/<int:todo_id>')
+def complete(todo_id):
+    todo_to_update = Todos.query.get(todo_id)
+    todo_to_update.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/incomplete/<int:todo_id>')
+def incomplete(todo_id):
+    todo_to_update = Todos.query.get(todo_id)
+    todo_to_update.complete = False
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:todo_id>', methods=['GET', 'POST'])
+def update(todo_id):
+    form = ToDoForm()
+    todo_to_update = Todos.query.get(todo_id)
+    if form.validate_on_submit():
+        todo_to_update.task = form.task.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        form.task.data = todo_to_update.task
+
+    return render_template('update.html', form=form)
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo_to_delete = Todos.query.get(todo_id)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
